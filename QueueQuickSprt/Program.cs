5d3e5caf886cce50a0365ea8adfd48e2757307e8
Alex Petrov <sasagromila@gmail.com>
2020-10-17 17:49:25 +0300

    Queue

--- QueueQuickSprt/Program.cs
+++ QueueQuickSprt/Program.cs
@@ -5,40 +5,49 @@ namespace Queue
 {
   public class QueueSort
   {
-    protected Node _head;
-    protected Node _tail;
+    Node _head;
+    Node _tail;
 
+    //count - кол-во элементов в очереди. Свойства: get - для чтения, private set - для записи только в этом классе
     public int Count { get; private set; } = 0;
 
+    //добавление элемента в очередь, data - его значение 
     public void Enqueue(int data)
     {
+      //создаем новый элемент
       Node _newNode = new Node(data);
+      //если очередь пуста, элемент добавляется в начало очереди
       if (IsEmpty())
       {
         _head = _newNode;
         _tail = _head;
       }
+      //иначе - в конец
       else
       {
         _tail.Next = _newNode;
         _tail = _tail.Next;
       }
+      //увеличиваем значение размера очереди
       Count++;
     }
+
+    //возвращение элемента из очереди
     public int Dequeue()
     {
       if (IsEmpty())
-        throw new Exception("Queue is Empty!");
-
+      {
+        Console.WriteLine("Queue is Empty!");
+        return 0;
+      }
       Count--;
+
       int _result = _head.Data;
       _head = _head.Next;
       return _result;
     }
-    public bool IsEmpty()
-    {
-      return _head == null;
-    }
+
+    //чтение первого элемента очереди
     public int Peek()
     {
       if (IsEmpty() == false)
@@ -50,17 +59,27 @@ namespace Queue
         return 0;
       }
     }
+
+    //проверка на пустоту очереди
+    public bool IsEmpty()
+    {
+      return _head == null;
+    }
+
+    //получение значения элемента по его индексу в очереди
     public int GetNumber(int Position)
     {
       if (IsEmpty() == false)
       {
         int ValueGet = 0;
 
+        //переборка всех значений очереди
         for (int i = 1; i < Count + 1; i++)
         {
+          //если цикл дошел до нужной позиции => в ValueGet записывается значение
           if (i == Position)
             ValueGet = Peek();
-
+          //если позиция не равна единице => 
           if (Position != 1)
             Enqueue(Dequeue());
           else
@@ -74,6 +93,8 @@ namespace Queue
         return 0;
       }
     }
+
+    //установка значения по индексу
     public void SetNumber(int Position, int ValueSet)
     {
       if (IsEmpty() == false)
@@ -108,8 +129,11 @@ namespace Queue
       else
         Console.WriteLine("Queue is Empty!");
     }
+
+    //быстрая сортировка
     public void QuickSort(int leftBorder, int rightBorder)
     {
+      //если левая граница больше правой, то программа прекращает работу
       if (leftBorder >= rightBorder)
         return;
 
@@ -147,6 +171,8 @@ namespace Queue
       if (right + 1 < rightBorder)
         QuickSort(right + 1, rightBorder);
     }
+
+    //метод замены местами
     public void Swap(int leftElement, int rightElement)
     {
       int temp = GetNumber(leftElement);
@@ -158,26 +184,39 @@ namespace Queue
   {
     static void Main()
     {
-      const int _digitsCount = 500;
+      Console.WriteLine("Quick queue sorting!");
+      //кол-во элементов очереди
+      const int _digitsCount = 200;
 
-      int[] KeyStore = new int[_digitsCount];
+      int[] key = new int[_digitsCount];
 
       QueueSort queue = new QueueSort();
       Random rnd = new Random();
 
+      //заполняем очередь ррандомными целыми числами
       for (int i = 0; i < _digitsCount; i++)
       {
-        KeyStore[i] = rnd.Next(1000);
-        queue.Enqueue(KeyStore[i]);
+        key[i] = rnd.Next(1000);
+        queue.Enqueue(key[i]);
       }
 
+      Console.WriteLine("_________________________________________________________");
+      Console.WriteLine($"Count of items in the queue: {queue.Count}");
+      Console.WriteLine("_________________________________________________________");
+
+      Console.WriteLine("Unsorted queue: ");
+      for (int i = 0; i < _digitsCount; i++)
+        Console.Write("{0,6}", key[i]);
+
+      //вызов сортировки
       queue.QuickSort(1, _digitsCount);
 
-      Console.WriteLine(queue.Count);
-      Console.WriteLine();
-
+      Console.WriteLine("Sorted queue: ");
       for (int i = 0; i < _digitsCount; i++)
         Console.Write("{0,6}", queue.Dequeue());
+
+
+      Console.WriteLine();
 
       Console.ReadLine();
     }
